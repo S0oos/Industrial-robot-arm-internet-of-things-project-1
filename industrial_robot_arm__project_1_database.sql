03a1bdafeaa22e27e04453899e7bc02e9a22e0dc
Wael Alghamdi <82511022+S0oos@users.noreply.github.com>
2021-06-18 00:29:28 +0300

    Add files via upload

---
+++ industrial_robot_arm__project_1_database.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 17, 2021 at 11:29 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `industrial_robot_arm__project_1_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `engines`
+--
+
+CREATE TABLE `engines` (
+  `id` int(11) NOT NULL,
+  `engineOne` int(3) NOT NULL,
+  `engineTwo` int(3) NOT NULL,
+  `engineThree` int(3) NOT NULL,
+  `engineFour` int(3) NOT NULL,
+  `engineFive` int(3) NOT NULL,
+  `engineSix` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `engines`
+--
+
+INSERT INTO `engines` (`id`, `engineOne`, `engineTwo`, `engineThree`, `engineFour`, `engineFive`, `engineSix`) VALUES
+(15, 105, 76, 100, 67, 81, 132),
+(16, 90, 90, 90, 90, 90, 90),
+(17, 90, 90, 90, 90, 90, 90),
+(18, 130, 90, 90, 90, 90, 125);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `run`
+--
+
+CREATE TABLE `run` (
+  `id` int(11) NOT NULL,
+  `robotOn` varchar(5) NOT NULL,
+  `robotOff` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `run`
+--
+
+INSERT INTO `run` (`id`, `robotOn`, `robotOff`) VALUES
+(19, 'on', 'null'),
+(20, 'null', 'off'),
+(21, 'on', 'null'),
+(22, 'null', 'off'),
+(23, 'on', 'null'),
+(24, 'null', 'off');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `engines`
+--
+ALTER TABLE `engines`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `run`
+--
+ALTER TABLE `run`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `engines`
+--
+ALTER TABLE `engines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `run`
+--
+ALTER TABLE `run`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
